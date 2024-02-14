4b53d60f955e4791380e49453d5e93f9e1fb4bae
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-02-15 00:04:23 +0200

    final commit + readme images

--- CardWorkoutGame/CardSelectionVC.swift
+++ CardWorkoutGame/CardSelectionVC.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    @IBOutlet var cardImageView: UIImageView!
+    var cards: [UIImage] = Deck.allValues
+    var timer: Timer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        startTimer()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
     }
-    */
-
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.15, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    
+    @IBAction func restartButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
+    }
+    
 }
